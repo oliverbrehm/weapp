8dcacc298ded6cba90d130713fe2ea69352710e1
Oliver Brehm <olibrehm@gmail.com>
2016-01-25 16:39:44 +0100

    initial ios incubation app commit

---
+++ incubation/ios/integrationsproject-incubateion/integrationsproject-incubateion/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  integrationsproject-incubateion
+//
+//  Created by Oliver Brehm on 25.01.16.
+//  Copyright © 2016 Oliver Brehm. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
